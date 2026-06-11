693238db409be39a01be773dfb5fcecb527753d2
xAlonLevi <xalonlevi@hotmail.com>
2026-06-11 20:20:38 +0300

    render img and txt func

--- js/main.js
+++ js/main.js
@@ -2,10 +2,49 @@
 
 console.log('hi');
 
+let gElCanvas
+let gCtx
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    renderMeme()
+}
+
+function renderMeme() {
+    const elImg = new Image()
+    elImg.src = gImgs[0].url
+    elImg.onload = () => renderImg(elImg)
+}   
+
+function renderImg(elImg) {
+    gElCanvas.height = (elImg.naturalHeight / elImg.naturalWidth) * gElCanvas.width
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+    renderText()
+}
+
+function renderText() {
+    const line = gMeme.lines[0]
+    gCtx.font = `${line.size}px Impact`
+  gCtx.fillStyle = line.color
+  gCtx.strokeStyle = 'white'
+  gCtx.lineWidth = 2
+  gCtx.textAlign = 'center'
+  gCtx.strokeText(line.txt, gElCanvas.width / 2, line.size + 10)
+  gCtx.fillText(line.txt, gElCanvas.width / 2, line.size + 10)
+}
+
+
+
 var gImgs = [
     {
         id: 1,
         url: 'img/1.jpg',
+        keywords: ['funny', 'cat']
+    },
+        {
+        id: 2,
+        url: 'img/2.jpg',
         keywords: ['funny', 'cat']
     }
 ]
