4a5db4cc2ff77d16c75c2da0e7dbb9936c949a52
xAlonLevi <xalonlevi@hotmail.com>
2026-06-14 19:25:44 +0300

    onclick img render by id. img flex box

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 'use strict'
 
-let input = document.getElementById('myInput')
+const input = document.getElementById('myInput')
 
 let gElCanvas
 let gCtx
 
 input.addEventListener('input', function () {
-gMeme.lines[0].txt = input.value
-renderMeme()
+    gMeme.lines[0].txt = input.value
+    renderMeme()
 })
-console.log(input.value);
 
 function onInit() {
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
+    renderGallery()
     renderMeme()
 }
 
 function renderMeme() {
     const elImg = new Image()
-    elImg.src = gImgs[1].url
+    elImg.src = gImgs[gMeme.selectedImgId].url
     elImg.onload = () => renderImg(elImg)
 }
 
@@ -41,7 +41,6 @@ function renderText() {
 }
 
 
-
 var gImgs = [
     {
         id: 1,
@@ -52,11 +51,31 @@ var gImgs = [
         id: 2,
         url: 'meme-imgs (square)/2.jpg',
         keywords: ['funny', 'cat']
-    }
+    },
+    {
+        id: 3,
+        url: 'meme-imgs (square)/3.jpg',
+        keywords: ['funny', 'cat']
+    },
+    {
+        id: 4,
+        url: 'meme-imgs (square)/4.jpg',
+        keywords: ['funny', 'cat']
+    },
+    {
+        id: 5,
+        url: 'meme-imgs (square)/5.jpg',
+        keywords: ['funny', 'cat']
+    },
+    {
+        id: 6,
+        url: 'meme-imgs (square)/6.jpg',
+        keywords: ['funny', 'cat']
+    },
 ]
 
 var gMeme = {
-    selectedImgId: 5,
+    selectedImgId: 0,
     selectedLineIdx: 0,
     lines: [
         {
@@ -69,4 +88,47 @@ var gMeme = {
 
 var gKeywordSearchCountMap = {
     'funny': 12, 'cat': 16, 'baby': 2
+}
+
+
+// const elSection = document.querySelector('section')
+
+// for (let i = 0; i < gImgs.length; i++) {
+//     const elImgGallery = new Image()
+
+//     elImgGallery.src = gImgs[i].url
+//     elSection.appendChild(elImgGallery)
+
+//     console.log(elImgGallery);
+// }
+
+let strHTML = ''
+
+function renderGallery() {
+    let strHTML = ''
+
+    for (let i = 0; i < gImgs.length; i++) {
+        strHTML += `
+            <img src="${gImgs[i].url}" 
+            onclick="onImgSelect(${i})">
+        `
+    }
+
+    document.querySelector('section').innerHTML = strHTML
+}
+
+
+// Add onClick to the gallery, it calls to onImgSelect =
+// this func updates gMeme with the clicked img and than 
+// call renderImg
+
+// function onImgSelect(elSelectedImg) {
+//     console.log(elSelectedImg);
+
+// }
+function onImgSelect(imgIdx) {
+    console.log(imgIdx)
+    gMeme.selectedImgId = imgIdx
+    renderMeme()
+
 }
